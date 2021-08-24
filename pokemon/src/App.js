93375a3cb516b9299428aae11a9b4b2b6aa935be
stefnotadevyet <srjack91@gmail.com>
2021-08-24 14:00:55 -0400

    added current next and previous page states

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -5,13 +5,18 @@ import axios from 'axios'
 
 function App() {
   const [pokemon, setPokemon] = useState([])
+  const [currentPage, setCurrentPage] = useState('https://pokeapi.co/api/v2/pokemon')
+  const [nextPage, setNextPage] = useState()
+  const [previousPage, setPreviousPage] = useState()
 
 
   useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon').then(res => {
+    axios.get(currentPage).then(res => {
+      setNextPage(res.data.next)
+      setPreviousPage(res.data.prec)
       setPokemon(res.data.results.map(p => p.name))
     })
-  }, [])
+  }, [currentPage])
 
   return (
   <PokeList pokemon={pokemon} />
